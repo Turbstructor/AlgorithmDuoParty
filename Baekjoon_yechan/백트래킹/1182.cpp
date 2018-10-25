aebef553da4bfbed9b809e152bcdbe6e0b95ee70
fbdp1202 <rbynddd@naver.com>
2018-10-25 18:55:51 +0900

    181025 19:00 update

---
+++ Baekjoon_yechan/백트래킹/1182.cpp
@@ -0,0 +1,26 @@
+// baekjoon 1182 yechan
+#include <iostream>
+using namespace std;
+
+int N, S, val[20];
+int cnt, currentSum;
+
+void dfs(int current){
+	if(current == N) return;
+	if(currentSum + val[current] == S) cnt++;
+
+	dfs(current+1);
+
+	currentSum += val[current];
+	dfs(current+1);
+
+	currentSum -= val[current];
+}
+
+int main(){
+	scanf("%d %d", &N, &S);
+	for(int i=0; i<N; i++)
+		scanf("%d", val+i);
+	dfs(0);
+	printf("%d\n", cnt);
+}
